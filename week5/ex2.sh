f471473e2547e6bc5b60ef88e9497756373422a8
Dmitry Chermnykh <27727194+Chermnyx@users.noreply.github.com>
2020-09-16 01:41:06 +0500

    Make ex2.sh wait for the resource instead of crashing

--- week5/ex2.sh
+++ week5/ex2.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-if [ -e ./num.lock ]; then
-	exit 1
-fi
+while ! ln -s ./num ./num.lock; do
+    sleep 0.01
+done
 
-if ln -s ./num ./num.lock; then
-	trap "rm ./num.lock" EXIT
+trap "rm ./num.lock" EXIT
 
-	# this script no longer could be in race condition
-	if [ -e ./num ]; then
-		n=$(cat ./num)
-		n=$((n + 1))
-		echo $n > ./num
-	else
-		echo 0 > ./num
-	fi
+# this script no longer could be in race condition
+if [ -e ./num ]; then
+	n=$(cat ./num)
+	n=$((n + 1))
+	echo $n > ./num
+else
+	echo 0 > ./num
 fi
