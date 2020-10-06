6a252eb09489ff23f887e1dee73ffe6be7d1599f
Dmitry Chermnykh <27727194+Chermnyx@users.noreply.github.com>
2020-10-06 17:30:40 +0500

    Fix formatting

--- week8/ex4.c
+++ week8/ex4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <sys/resource.h>
+#include <time.h>
 
 #define MB10 (10 * 1000 * 1000)
 
@@ -13,8 +13,8 @@ int main(int argc, char const* argv[]) {
     void* mem = malloc(MB10);
     memset(mem, 0, MB10);
 
-		getrusage(RUSAGE_SELF, &usage);
-		printf("ru_maxrss: %ld\n", usage.ru_maxrss);
+    getrusage(RUSAGE_SELF, &usage);
+    printf("ru_maxrss: %ld\n", usage.ru_maxrss);
 
     sleep(1);
   }
